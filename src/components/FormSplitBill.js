03391b02b9b7ed83bf2a42b9ed3bd94ecc66cdb6
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-10-31 14:38:32 +0700

    🚧 Create controlled elements

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,21 +1,43 @@
+import { useState } from "react";
+
 import Button from "./Button";
 
 export default function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+  const paidByFriend = bill ? bill - paidByUser : "";
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(event) => setBill(+event.target.value)}
+      />
 
       <label>🤵 Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(event) =>
+          setPaidByUser(
+            +event.target.value > bill ? paidByUser : +event.target.value
+          )
+        }
+      />
 
       <label>👫 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" value={paidByFriend} disabled />
 
       <label>🤑 Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(event) => setWhoIsPaying(event.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
